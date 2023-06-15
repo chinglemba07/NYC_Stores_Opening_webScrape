2930667160681ae06a0dfbcf6d26e586c1bd2eac
Chinglemba <130333499+chinglemba07@users.noreply.github.com>
2023-06-15 13:07:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+
+
+stores = []
 
 url = 'https://ny.eater.com/restaurant-openings'
 
 response = requests.get(url)
-# print(response)
 soup = BeautifulSoup(response.content, 'html.parser')
 # print(soup)
 shop_elements = soup.find_all('div', class_='c-entry-box--compact__body')
@@ -20,17 +23,31 @@ for shop in shop_elements:
     shop_name = shop_head.find('a').text
     
 
-    print(shop_name)
+    # print(shop_name)
     # print(shop_url)
 
 
     author = shop.find('span', class_='c-byline__author-name')
     author = author.text
-    print(f"by {author}")
+    # print(f"by {author}")
 
     time = shop.find('time', class_='c-byline__item')
     time = time.get_text(strip=True)
-    print(time)
+    # print(time)
 
-    print(link)
+    # print(link)
     print('\n')
+
+    #appending the details to the dict
+    stores.append({
+        "name": shop_name,
+        "author": author,
+        "time": time,
+        "shop_url": link
+    })
+
+for shop in stores:
+    print(shop,"\n")
+
+with open('stores.json', 'w') as json_file:
+    json.dump(stores, json_file)
